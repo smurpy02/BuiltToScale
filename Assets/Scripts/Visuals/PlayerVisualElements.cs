2d0e89243ae0b42e2e38bf6d7d3477f56448a333
smurpy02 <scottmurphy.wales@gmail.com>
2026-09-15 22:36:38 +0100

    Procedural Slime PART 2 THE FINALE

--- Assets/Scripts/Visuals/PlayerVisualElements.cs
+++ Assets/Scripts/Visuals/PlayerVisualElements.cs
@@ -1,26 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.U2D;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Tilemaps;
 using UnityEngine.U2D;
+using Random = UnityEngine.Random;
 
 public class PlayerVisualElements : MonoBehaviour
 {
     public Sprite defaultSprite, surroundedSprite;
-    public SpriteRenderer spriteRenderer;
-    public SpriteAtlas playerAtlas;
-    public bool tryLooseFit = true, usePresetSquareSprites = false;
+    public SpriteRenderer playerRenderer, speckRenderer;
+    public SpriteAtlas playerAtlas, speckAtlas;
+    public Texture2D playerTexture;
 
-    List<Sprite> playerSprites = new List<Sprite>();
     [SerializeField] List<SquareSprite> squareSprites = new List<SquareSprite>();
     List<Vector2Int> blockDirections = new List<Vector2Int>();
-
-    static Dictionary<SpriteAtlas, List<SquareSprite>> squareSpritesShared = new();
 
     private List<Vector2Int> directions = new List<Vector2Int>
     {
@@ -28,96 +22,84 @@ public class PlayerVisualElements : MonoBehaviour
         Vector2Int.down,
         Vector2Int.right,
         Vector2Int.left,
-        Vector2Int.up + Vector2Int.left,
-        Vector2Int.up + Vector2Int.right,
-        Vector2Int.down + Vector2Int.left,
-        Vector2Int.down + Vector2Int.right,
     };
 
     void Start()
     {
-        InitSquareSprites();
+        CreateSpawnSprites();
+        RandomiseSpeck();
         UpdateSprite();
     }
 
-    void InitSquareSprites()
+    void RandomiseSpeck()
     {
-        if (usePresetSquareSprites)
+        var sprites = GetSprites(speckAtlas);
+
+        if (sprites.Count == 0) return;
+
+        speckRenderer.sprite = sprites[Random.Range(0, sprites.Count)];
+    }
+
+    void CreateSpawnSprites()
+    {
+        var sprites = GetSprites(playerAtlas);
+
+        if (sprites.Count == 0) return;
+
+        squareSprites.Clear();
+        var jointColour = GetJointColor();
+
+        Debug.Log("===" + gameObject.GetEntityId());
+        Debug.Log(jointColour.ToString());
+        Debug.Log("-----");
+
+        foreach(var sprite in sprites)
         {
-            return;
-        }
-
-        if (squareSpritesShared.ContainsKey(playerAtlas))
-        {
-            squareSprites = squareSpritesShared[playerAtlas];
-            return;
-        }
-
-        InitializePlayerSprites();
-
-        Vector2 textureSize = playerSprites[0].texture.Size();
-
-        Color jointColour = surroundedSprite.texture.GetPixel((int)surroundedSprite.rect.x, (int)surroundedSprite.rect.y);
-
-        int spriteIndex = 0;
-
-        foreach (var sprite in playerSprites)
-        {
-            spriteIndex++;
-
-            var spriteTexture = sprite.texture;
-            var spriteRect = sprite.rect;
-
             var squareSprite = new SquareSprite(sprite);
 
-            foreach (var direction in directions)
+            Debug.Log(sprite.name);
+            Debug.Log("..........");
+
+            foreach(var direction in directions)
             {
-                int textureX = (int)spriteRect.x;
+                var color = GetPixelColor(sprite, direction);
 
-                if (direction.x == 0) textureX += 7;
-                if (direction.x > 0) textureX += 15;
-
-                int textureY = (int)spriteRect.y;
-
-                if (direction.y == 0) textureY += 7;
-                if (direction.y > 0) textureY += 15;
-
-                var colour = spriteTexture.GetPixel(textureX, textureY);
-
-                if (colour.Equals(jointColour)) squareSprite.blockDirections.Add(direction);
+                if (color.Equals(jointColour)) squareSprite.blockDirections.Add(direction);
             }
 
             squareSprites.Add(squareSprite);
         }
-
-        squareSpritesShared.Add(playerAtlas, squareSprites);
     }
 
-    void InitializePlayerSprites()
+    Color GetPixelColor(Sprite sprite, Vector2Int direction)
     {
-        Sprite[] spriteArray = new Sprite[playerAtlas.spriteCount];
+        var rect = sprite.rect.center;
+        var texture = playerTexture;
 
-        playerAtlas.GetSprites(spriteArray);
+        if (direction.x < 0) rect.x -= 8;
+        if (direction.x > 0) rect.x += 7;
 
-        playerSprites = spriteArray.ToList();
+        if (direction.y < 0) rect.y -= 8;
+        if (direction.y > 0) rect.y += 7;
 
-        Debug.Log(playerSprites.Count);
+        Debug.Log(texture.Size());
+        Debug.Log("rect " + rect.x + " " + rect.y);
+
+        return texture.GetPixel((int)rect.x, (int)rect.y);
     }
 
-    public void PopulateSquareSprites()
+    Color GetJointColor()
     {
-        Debug.Log("Populating!");
+        return GetPixelColor(surroundedSprite, Vector2Int.up);
+    }
 
-        InitializePlayerSprites();
+    List<Sprite> GetSprites(SpriteAtlas atlas)
+    {
+        Sprite[] sprites = new Sprite[atlas.spriteCount];
 
-        squareSprites.Clear();
+        atlas.GetSprites(sprites);
 
-        foreach (var sprite in playerSprites)
-        {
-            Debug.Log(sprite);
-
-            squareSprites.Add(new SquareSprite(sprite));
-        }
+        return sprites.ToList();
     }
 
     public void BlockRemoved(Vector2Int myPosition, Vector2Int blockPosition)
@@ -142,24 +124,12 @@ public class PlayerVisualElements : MonoBehaviour
         {
             if (SpriteStrictlyPasses(squareSprite))
             {
-                spriteRenderer.sprite = squareSprite.sprite;
+                playerRenderer.sprite = squareSprite.sprite;
                 return;
             }
         }
 
-        if (tryLooseFit)
-        {
-            foreach (var squareSprite in squareSprites)
-            {
-                if (SpriteLooselyPasses(squareSprite))
-                {
-                    spriteRenderer.sprite = squareSprite.sprite;
-                    return;
-                }
-            }
-        }
-
-        spriteRenderer.sprite = defaultSprite;
+        playerRenderer.sprite = defaultSprite;
     }
 
     bool SpriteStrictlyPasses(SquareSprite sprite)
@@ -167,28 +137,6 @@ public class PlayerVisualElements : MonoBehaviour
         if (
             sprite.blockDirections.All(direction => blockDirections.Contains(direction)) &&
             blockDirections.All(direction => sprite.blockDirections.Contains(direction))
-            )
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    bool SpriteLooselyPasses(SquareSprite sprite)
-    {
-        if
-            (
-            sprite.blockDirections.All
-                (
-                direction => (direction.x == 0 || direction.y == 0) &&
-                blockDirections.Contains(direction)
-                ) &&
-            blockDirections.All
-                (
-                direction => (direction.x != 0 && direction.y != 0) ||
-                sprite.blockDirections.Contains(direction)
-                )
             )
         {
             return true;
